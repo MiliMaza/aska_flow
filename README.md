81dbe0935d43f29fe9db770e568fbd0756ae4abd
MiliMaza <milagrosmazamm@gmail.com>
2025-09-02 06:43:01 -0300

    Small-first changes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 ## Getting Started
 
 First, run the development server:
@@ -13,5 +14,3 @@ bun dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`.
--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,15 @@
 import { useChat } from "@ai-sdk/react";
 import { useState } from "react";
 
-export default function Chat() {
+export default function Home() {
   const [input, setInput] = useState("");
   const { messages, sendMessage } = useChat();
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages.map((message) => (
         <div key={message.id} className="whitespace-pre-wrap">
-          {message.role === "user" ? "User: " : "AI: "}
+          {/* Replace "User" with the username or just the user image */}
+          {message.role === "user" ? "User: " : "Aska: "}
           {message.parts.map((part, i) => {
             switch (part.type) {
               case "text":
@@ -28,9 +29,9 @@ export default function Chat() {
         }}
       >
         <input
-          className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
+          className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-foreground dark:border-zinc-800 rounded shadow-xl"
           value={input}
-          placeholder="Say something..."
+          placeholder="Describe the automation you want to get..."
           onChange={(e) => setInput(e.currentTarget.value)}
         />
       </form>
