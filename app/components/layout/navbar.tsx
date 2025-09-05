72aa7c7e4e1dcdf31801da5fa5163344d7801bd4
MiliMaza <milagrosmazamm@gmail.com>
2025-09-05 12:14:22 -0300

    Adding navbar

--- app/components/layout/navbar.tsx
+++ app/components/layout/navbar.tsx
@@ -1,21 +1,38 @@
-import Image from 'next/image';
+"use client";
+
+import Image from "next/image";
+import { Menu } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between px-4 py-4 bg-accent shadow-md">
-      <div className="flex items-center space-x-2">
-        <Image src="/Logo.png" alt="AskaFlow logo" width={32} height={32} />
-        <span 
-            className="font-semibold text-2xl text-background mx-2 hover:scale-105">AskaFlow</span>
+    <nav className="bg-secondary">
+      <div className="flex items-center justify-between p-5">
+        {/* Logo and Name */}
+        <div className="flex items-center gap-4">
+          <button className="">
+            <Menu className="w-6 h-6" color="#f7f7ffff" />
+          </button>
+          <div className="flex items-center gap-4">
+            <Image
+              src="/Logo.png"
+              alt="Aska Flow Logo"
+              width={32}
+              height={32}
+            />
+            <span className="text-2xl font-bold text-background">
+              ASKA <span className="font-semibold">FLOW</span>
+            </span>
+          </div>
+        </div>
+
+        {/* User profile */}
+        <div className="flex items-center gap-4">
+          {/* TODO: Implement Clerk Profile Button */}
+          <div className="w-8 h-8 rounded-full bg-accent flex items-center justify-center text-background font-semibold">
+            U
+          </div>
+        </div>
       </div>
-      <a
-        href="https://docs.n8n.io/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-md text-background hover:underline"
-      >
-        n8n Docs
-      </a>
     </nav>
   );
 }
